3c54273d59ad8ed7f56f2a89034392fc531c9c1a
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-04-12 03:35:33 -0500

    starting from App.js and fetching data from back end db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    postData: []
+  };
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  fetchPosts = () => {
+    fetch(`https://localhost:3000/posts`)
+      .then(resp => resp.json())
+      .then(data => console.log("data:", data));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to drewsList!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
